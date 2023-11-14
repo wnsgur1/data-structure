fbedbc803ffcd2a7ff6bbe0a4bb75acb322c2aca
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-11-14 13:59:01 +0900

    selcetion_sort

--- 1219한준혁.py
+++ 1219한준혁.py
@@ -1,21 +1,35 @@
 import random
 
-def selcetion():
-    print("selcetion")
+# 선택 정렬
+def selcetion(list):
+    # 리스트 만큼 반복
+    for i in range(len(list)):
+        # 첫번째 인덱스
+        min = i
+        # i+1 부터 리스트 크기전까지 반복
+        for j in range(i+1, len(list)):
+            #만약 min 인덱스 다음에 있는 수가 min 인덱스 에 있는 값보다 작은게 있으면 
+            if list[j] < list[min]:
+                # min은 작은 값의 인덱스
+                min = j
+        # 첫번째 인덱스의 값과 가장 작은 값을 교체
+        list[i], list[min] = list[min], list[i]
+    # 리턴
+    return list
 
-def insertion():
+def insertion(list):
     print("insertion")
 
-def bubble():
+def bubble(list):
     print("bubble")
     
-def quick():
+def quick(list):
     print("quick")
 
-def merge():
+def merge(list):
     print("merge")
 
-def heap():
+def heap(list):
     print("heap")
 
 
@@ -29,17 +43,29 @@ def main():
         if a in num:
             match a:
                 case 1:
-                    selcetion()
+                    print("선택 정렬")
+                    print("정렬 전", list)
+                    print("정렬 후", selcetion(list))                      
                 case 2:
-                    insertion()
+                    print("삽입 정렬")
+                    print("정렬 전", list)
+                    print("정렬 후", insertion(list))                  
                 case 3:
-                    bubble()
+                    print("버블 정렬")
+                    print("정렬 전", list)
+                    print("정렬 후", bubble(list))
                 case 4:
-                    quick()
+                    print("퀵 정렬")
+                    print("정렬 전", list)
+                    print("정렬 후", quick(list))
                 case 5:
-                    merge()
+                    print("합병 정렬")
+                    print("정렬 전", list)
+                    print("정렬 후", merge(list))
                 case 6:
-                    heap()
+                    print("힙 정렬")
+                    print("정렬 전", list)
+                    print("정렬 후", heap(list))
                 case 7:
                     print("종료")
                     break
